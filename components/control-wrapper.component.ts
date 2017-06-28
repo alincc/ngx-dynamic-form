56504ee37acae9b4d699e2c3265a9feaf5c2721d
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-28 00:23:30 -0500

    create control-wrapper component

---
+++ components/control-wrapper.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Control } from "app/dynamic-form/models/control";
+import { ControlConfig } from "app/dynamic-form/models/control-config";
+import { FormGroup } from "@angular/forms";
+
+@Component({
+  selector: 'control-wrapper',
+  template: 
+    `<div
+      class="form-group"
+      [class.has-success]="group.get(config.name).valid && group.get(config.name).touched"
+      [class.has-error]="(!group.get(config.name).valid && group.get(config.name).touched) || errors[config.name]"
+      [ngClass]="[config.mainWrapperClass || '']">
+
+      <label
+        class="control-label"
+        [attr.for]="config.name"
+        [ngClass]="[config.labelClass || '']">
+        {{ config.label }}
+      </label>
+
+      <div [ngClass]="[config.controlWrapperClass || '']">
+
+        <ng-content></ng-content>
+        
+      </div>
+    </div>`
+})
+export class ControlWrapperComponent implements Control, OnInit {
+  @Input()
+  public config: ControlConfig;
+
+  @Input()
+  public group: FormGroup;
+
+  @Input()
+  public errors: any;
+
+  @Input()
+  public disabled: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() { }
+}
